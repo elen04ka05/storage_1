009c1ad63daf5ff64a34d310297b23021b2420f0
Juliakuznetsovaaaa <jyliakuz@gmail.com>
2024-01-03 15:57:52 +0300

    add profile page, edit welcome, sign_up, sign_in

--- taskmanager/main/static/JS/navigation_sign_up.js
+++ taskmanager/main/static/JS/navigation_sign_up.js
@@ -1,3 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const signUpForm = document.getElementById('signup-form');
+
+    function registerUser(event) {
+        event.preventDefault(); // Prevent the default form submission behavior
+        const username = document.getElementById('username').value;
+        const queryString = "?username=" + username;
+        window.location.href = "profile.html" + queryString;
+    }
+
+    signUpForm.addEventListener('submit', registerUser); // Register the registerUser function for form submission
+});
+
 const sign_up_page_btn = document.getElementById('sign_in_page_btn');
 
 sign_up_page_btn.addEventListener('click', () => {
