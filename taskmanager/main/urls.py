dd8aef3612b7956823118ad7450d168ca1b521f6
Victoria Tabakova <vrtabakova@gmail.com>
2024-01-03 14:59:08 +0300

    Fix tailwind styles and update home page

--- taskmanager/main/urls.py
+++ taskmanager/main/urls.py
@@ -5,7 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('', views.welcome),#welcome
     # path('sign_in/', TemplateView.as_view(template_name='main/sign_in.html'), views.sign_in),
-    path('sign_in.html', views.sign_in),
-    path('sign_up.html', views.sign_up),
-    path('home_page.html', views.home_page),
+    path('sign_in', views.sign_in),
+    path('sign_up', views.sign_up),
+    path('home_page', views.home_page),
 ]
